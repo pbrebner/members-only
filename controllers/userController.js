d9ef24998a66031b1312e4b21df21eac289f2de5
Patrick Brebner <patrick.brebner@gmail.com>
2023-11-30 20:44:17 -0800

    Add passport and log-in controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,12 +2,52 @@ const User = require("../models/user");
 
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
 
-// Display Sign-Up Form on Get
-exports.signUpGet = asyncHandler(async (req, res, next) => {
-    res.render("sign-up", { title: "Sign Up Form" });
+// Set up passport to authenticate login
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        console.log("running");
+        try {
+            const user = await User.findOne({ email: username });
+            console.log(user);
+            if (!user) {
+                console.log("username");
+                return done(null, false, { message: "Incorrect username" });
+            }
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) {
+                // passwords do not match!
+                console.log("password");
+                return done(null, false, { message: "Incorrect password" });
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
 });
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
+
+// Display Sign-Up Form on Get
+exports.signUpGet = (req, res, next) => {
+    res.render("sign-up", { title: "Sign Up Form" });
+};
 
 // Handle Sign-up Form Post
 exports.signUpPost = [
@@ -20,7 +60,19 @@ exports.signUpPost = [
         .trim()
         .isLength({ min: 1 })
         .escape(),
-    body("email", "Must use valid email").trim().isEmail().escape(),
+    body("email", "Must use valid email")
+        .trim()
+        .isEmail()
+        .custom(async (value) => {
+            const user = await User.find({ email: value });
+            console.log(user);
+            if (user.length > 0) {
+                throw new Error(
+                    "Email is already in use, please use a different one"
+                );
+            }
+        })
+        .escape(),
     body("password", "Password must not be empty")
         .trim()
         .isLength({ min: 5 })
@@ -60,11 +112,23 @@ exports.signUpPost = [
 ];
 
 // Display Log-In on Get
-exports.logInGet = asyncHandler(async (req, res, next) => {
-    res.send("Not implemented Yet");
-});
+exports.logInGet = (req, res, next) => {
+    res.render("log-in", { title: "Log In" });
+};
 
 // Handle Log-In Post
-exports.logInPost = asyncHandler(async (req, res, next) => {
-    res.send("Not implemented Yet");
+exports.logInPost = passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/log-in",
+    failueMessage: true,
 });
+
+// Handle Log-Out
+exports.logOutGet = (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/");
+    });
+};
