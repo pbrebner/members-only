cb1dd465bf9e2d46b183339e9081c1151aaa8283
Patrick Brebner <patrick.brebner@gmail.com>
2023-11-30 16:01:07 -0800

    Add routes for home/sign-up/log-in

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// Require controller modules.
+const messageController = require("../controllers/messageController");
+const userController = require("../controllers/userController");
+
+// MESSAGE ROUTES
+
+// GET home page
+router.get("/", messageController.index);
+
+// USER ROUTES
+
+// Get sign-up page
+router.get("/sign-up", userController.signUpGet);
+
+// Post sign-up page
+router.post("/sign-up", userController.signUpPost);
+
+// Get log-in page
+router.get("/log-in", userController.logInGet);
+
+// Post log-in page
+router.post("/log-in", userController.logInPost);
 
 module.exports = router;
