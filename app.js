642117eacbc2b5c3ce5a304d511d2d27b4bba9f3
Patrick Brebner <patrick.brebner@gmail.com>
2023-12-02 19:07:03 -0800

    Set-up for deployment

--- app.js
+++ app.js
@@ -10,7 +10,22 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
+const compression = require("compression");
+const helmet = require("helmet");
+
 const app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// Add helmet to the middleware chain.
+app.use(helmet());
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
@@ -39,6 +54,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Set up to be able to access currentUser from local variables
