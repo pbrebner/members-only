95c869e5eb6d2653d3a4308674d4675b456cacfb
Patrick Brebner <patrick.brebner@gmail.com>
2023-12-04 10:13:56 -0800

    Remove unnecessary routes

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const compression = require("compression");
 const helmet = require("helmet");
@@ -64,7 +63,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
