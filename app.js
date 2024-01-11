c386b51fb7d3f00168d32112262f513a3345ea3f
Patrick Brebner <patrick.brebner@gmail.com>
2024-01-10 17:09:05 -0800

    Increase rate limit to 60 per minute

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ const helmet = require("helmet");
 
 const app = express();
 
-// Set up rate limiter: maximum of twenty requests per minute
+// Set up rate limiter: maximum of sixty requests per minute
 const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
     windowMs: 1 * 60 * 1000, // 1 minute
-    max: 20,
+    max: 60,
 });
 // Apply rate limiter to all requests
 app.use(limiter);
